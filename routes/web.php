6abd5cca46ae7e1ba3a8c3c4fb7a192dc434150f
GilbranFahmi <gilbranlaptop@gmail.com>
2026-01-28 22:21:39 +0200

    update routes2

--- routes/web.php
+++ routes/web.php
@@ -4,32 +4,26 @@ use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 
-// HOME
 Route::get('/', function () {
     return view('home');
 })->name('home');
 
-// CONTACT
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-// KATALOG
 Route::get('/katalog', function () {
     return view('katalog');
 })->name('katalog');
 
-// KATALOG DETAIL
 Route::get('/katalog-details', function () {
     return view('katalog-details');
 })->name('katalog.details');
 
-// BERITA
 Route::get('/berita', function () {
     return view('berita');
 })->name('berita');
 
-// DETAIL BERITA
 Route::get('/detailberita', function () {
     return view('detailberita');
 })->name('berita.detail');
